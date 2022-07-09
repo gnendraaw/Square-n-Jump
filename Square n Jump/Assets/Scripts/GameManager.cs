33f1d25daaab393b586239f741ab2341c676bc10
gnendraaw <itsgnendraaw@gmail.com>
2022-07-09 11:11:33 +0800

    add score point system & setup shop UI

--- Square n Jump/Assets/Scripts/GameManager.cs
+++ Square n Jump/Assets/Scripts/GameManager.cs
@@ -6,11 +6,39 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager singleton;
 
+    private int playerPoint;
+
     private void Start()
     {
-        singleton = this;
+        setup();
     }
 
+    void setup()
+    {
+        singleton = this;
+        setPlayerPoint(PlayerPrefs.GetInt("PlayerPoint"));
+    }
+
+    public void setPlayerPoint(int value)
+    {
+        playerPoint = value;
+    }
+
+    public int getPlayerPoint()
+    {
+        return playerPoint;
+    }
+        
+    public void addPoint()
+    {
+        setPlayerPoint(getPlayerPoint() + 1);
+    }
+
+    public void savePlayerPoint()
+    {
+        PlayerPrefs.SetInt("PlayerPoint", getPlayerPoint());
+    }
+        
     public int getSelectedChar()
     {
         return PlayerPrefs.GetInt("SelectedChar");
